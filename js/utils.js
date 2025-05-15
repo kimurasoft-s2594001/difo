e701b8b3aa32e454612bbf4a53c996321b57e278
kimurasoft-s2594001 <92353333+kimurasoft-s2594001@users.noreply.github.com>
2025-05-16 02:22:01 +0900

    add comment

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,10 @@
-// 防抖函数：事件被触发n秒后才执行，如果期间再次触发则重新计时
+/**
+ * 防抖函数：事件被触发n秒后才执行，如果期间再次触发则重新计时
+ * 注意：目前未在项目中使用，保留用于可能的未来需求
+ * @param {Function} func 需要执行的函数
+ * @param {number} delay 延迟时间，单位毫秒
+ * @returns {Function} 防抖处理后的函数
+ */
 export function debounce(func, delay = 300) {
   let timer = null;
   return function (...args) {
